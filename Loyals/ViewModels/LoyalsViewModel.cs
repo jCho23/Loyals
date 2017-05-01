52d3086b9316a3148695d163c350b063279754ef
JuneCho23 <junecho.ucb@gmail.com>
2017-05-01 16:02:59 -0700

    Properties/ Get and Set

---
+++ Loyals/ViewModels/LoyalsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Loyals
+{
+	public class LoyalsViewModel : BaseViewModel
+	{
+		public LoyalsViewModel()
+		{
+		}
+
+		private string pointsValue;
+		public string PointsValue
+		//properties ALWAYS has a GET or SET
+		{
+			get { return pointsValue; }
+			set
+			{
+				pointsValue = value;
+
+
+				OnPropertyChanged();
+			}
+		}
+
+
+
+	}
+}
