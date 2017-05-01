1076dade78295e7803b9b16bc1e2fe271a05427c
JuneCho23 <junecho.ucb@gmail.com>
2017-05-01 15:52:32 -0700

    This is where we put in Attributes for onProperty Changed

---
+++ Loyals/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace Loyals
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public BaseViewModel()
+		{
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged([CallerMemberName] //This is an Attribute, always in Square Brackets
+		                                         string propertyName=null)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this,
+					new PropertyChangedEventArgs(propertyName));
+			}attribute
+		}
+	}
+}
