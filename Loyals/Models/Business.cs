323ade9e68a7e66c14649c7aabe9d8672c7ebd5d
JuneCho23 <junecho.ucb@gmail.com>
2017-05-31 10:24:24 -0700

    typeOf

--- Loyals/Models/Business.cs
+++ Loyals/Models/Business.cs
@@ -3,8 +3,20 @@ namespace Loyals.Models
 {
     public class Business
     {
-        public Business()
-        {
+		public Business(string name, DateTime lastVisit)
+		{
+			this.Name = name;
+			this.LastVisit = lastVisit;
+		}
+
+		public string Name 
+        //Name is a type of 'String'
+        { 
+            private set; 
+            get; 
         }
+
+		public DateTime LastVisit { private set; get; }
+        //LastVisit is a type of DateTime
     }
 }
