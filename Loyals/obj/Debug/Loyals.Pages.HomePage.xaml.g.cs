897533582f3ce90726c0665182ebf4ca06e6e4c2
JuneCho23 <junecho.ucb@gmail.com>
2017-05-31 18:21:11 -0700

    xaml

--- Loyals/obj/Debug/Loyals.Pages.HomePage.xaml.g.cs
+++ Loyals/obj/Debug/Loyals.Pages.HomePage.xaml.g.cs
@@ -15,8 +15,12 @@ namespace Loyals {
     public partial class HomePage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.ListView list;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(HomePage));
+            list = global::Xamarin.Forms.NameScopeExtensions.FindByName <global::Xamarin.Forms.ListView>(this, "list");
         }
     }
 }
