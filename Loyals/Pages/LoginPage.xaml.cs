19cc102fc974d80cdab31efa3958495523219135
JuneCho23 <junecho.ucb@gmail.com>
2017-08-05 20:59:51 -0700

    LoginPage CB File Btn.Clicked

--- Loyals/Pages/LoginPage.xaml.cs
+++ Loyals/Pages/LoginPage.xaml.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
+
 
 namespace Loyals
 {
     public partial class LoginPage : ContentPage
     {
+        public event Action<Account> LoginSucceeded = (x) => { };
+        AccountManager accountManager;
+
         public LoginPage()
         {
             InitializeComponent();
+
+            accountManager = new AccountManager();
+
+            btnLogin.Clicked += BtnLoginClicked;
+            btnCreateAccount.Clicked += BtnCreateAccountClicked;
         }
+
+
+
+
+
+
+
     }
 }
