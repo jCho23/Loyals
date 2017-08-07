2e87c43cefa66ed4706cd0a2518de84fac6a6698
JuneCho23 <junecho.ucb@gmail.com>
2017-08-06 19:46:57 -0700

    Added MyTheme.Splash in Values

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -11,8 +11,13 @@ using Android.OS;
 
 namespace Loyals.Droid
 {
-	[Activity(Label = "Loyals.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	[Activity(Label = "Loyals.Droid", 
+                Icon = "@drawable/icon", 
+                Theme = "@style/MyTheme.Splash", 
+                MainLauncher = true,
+                ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
 		{
