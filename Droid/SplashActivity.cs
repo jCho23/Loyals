c04c1a2b990a3310ce0363cfb0ffb5d50a60493b
JuneCho23 <junecho.ucb@gmail.com>
2017-08-06 19:04:49 -0700

    using statements in SplashActivity

--- Droid/SplashActivity.cs
+++ Droid/SplashActivity.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 
